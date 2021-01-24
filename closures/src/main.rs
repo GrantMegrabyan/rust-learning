f0b4dbc721c7ab00c3dad3ea954303e81df027ab
Grant Megrabyan <grant.megrabyan@gmail.com>
2021-01-24 20:33:20 +0000

    Closures exercise

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -18,6 +18,9 @@ fn main() {
 
     explicit_move();
     println!("{}", delim);
+
+    exercise();
+    println!("{}", delim);
 }
 
 fn simple_closure() {
@@ -144,6 +147,9 @@ fn explicit_move() {
             println!("Hello, {}", name)
         }
     };
+    // Here we need to pass the closure by reference otherwise
+    // the ownership will be moved into the 'call_fn' not letting us 
+    // use it again.
     call_fn(&say_hi);
     call_fn_mut(&say_hi);
     call_fn_once(&say_hi);
@@ -168,4 +174,14 @@ where
     F: FnOnce(),
 {
     f()
+}
+
+fn exercise() {
+    let nums: Vec<u32> = (1..11).collect();
+
+    for _ in 1..3 {
+        for i in nums.iter().map(|n: &u32| n * 2) {
+            println!("{}", i);
+        }
+    }
 }
